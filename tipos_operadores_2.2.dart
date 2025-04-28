adb57d40749f313b3ad1df4c5fa3da6bb2bf10d5
Lucaslps10 <134088987+Lucaslps10@users.noreply.github.com>
2025-04-28 17:36:23 +0000

    28/04

---
+++ tipos_operadores_2.2.dart
@@ -0,0 +1,15 @@
+void main() {
+    int idade = 28;
+    double altura = 1.75;
+    String animalEstimacao = "Jubileu";
+    bool acenderLampada = false;
+    int valor1 = 10, valor2 = 20;
+    var valor3 = valor1 + valor2;
+    var resto = valor1 % valor2;
+    double compra = 100.00;
+    var compraComDesconto = 100*(10/100);
+
+    String celular = "Vivo V50";
+    int preco = 2800;
+    print("Um celular $celular custa em média $preco;");
+}
