0127af04aa8fd17a20b7e7200b2a4377eb2508bd
Lucaslps10 <134088987+Lucaslps10@users.noreply.github.com>
2025-05-08 18:16:55 +0000

    08/05

---
+++ exemplos.dart
@@ -0,0 +1,21 @@
+class Carro{
+  String _marca;
+  String _modelo;
+  int _ano;
+
+  Carro(this._marca, this._modelo, this._ano);
+
+  String get marca => _marca;
+  String get modelo => _modelo;
+  int get ano => _ano;
+
+  set marca(String nome){
+      _marca = nome;
+  }
+  set modelo(String nome){
+      _modelo = nome;
+  }
+  set ano(int valor){
+    _ano = valor;
+  }
+}
