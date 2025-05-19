d8bd8d051bd3885504883bb32c3435e78f534440
Lucaslps10 <134088987+Lucaslps10@users.noreply.github.com>
2025-05-19 17:46:00 +0000

    19/05

--- subtopico_2_2.dart
+++ subtopico_2_2.dart
@@ -1,34 +1,44 @@
 void main() {
+    /////////// 1 ///////////
     int idade = 28;
     print("Idade: $idade");
 
+    ////////// 2 ///////////
     double altura = 1.75;
     print("Altura: $altura");
 
+    ////////// 3 //////////
     String animalEstimacao = "Jubileu";
     print("Animal de estimação: $animalEstimacao");
 
+    ///////// 4 //////////
     bool acenderLampada = false;
     print("Lâmpada ligada: $acenderLampada");
 
+    ///////// 5 ///////////
     int a = 10, b = 20;
     var soma = a + b;
     print("$a, $b, Soma: $soma");
 
+    ///////// 6 ////////////
     var resto = a % b;
     print("Resto da divisão entre 'a' e 'b': $resto");
 
+    ///////// 7 ///////////
     double compra = 100.00;
     var compraComDesconto = compra - compra * (10/100);
     print("Compra: $compra \nCompra com desconto de 10%: $compraComDesconto");
 
+    ///////// 8 ///////////
     String celular = "Vivo V50";
     int preco = 2800;
     print("Um celular $celular custa em média $preco;");
 
+    ///////// 9 ////////////
     var nomeCidades = ["Picuí", "Carnaúba", "Nova Palmeira"];
     print("Segunda cidade da lista: " + nomeCidades[1]);
 
+    ///////// 10 ////////////
     var frutas = {
         'Maçã' : 3.0,
         'Banana' : 8.0,
@@ -39,28 +49,31 @@ void main() {
     print(frutas.keys);
     print("Valor da banana: ${frutas['Banana']}");
 
+    ///////// 11 //////////
     var conjunto = <String> {'1', '2', '3', '2'};
  
     var comprimento = conjunto.length;
     print("Valor do conjunto: $conjunto");
     print("Comprimento do conjunto: $comprimento");
 
+    /////////// 12 ////////
     dynamic valor = 20;
     print("Variável dinâmica inteira: $valor");
     valor = "Hello";
     print("Mudando o valor para String: $valor");
 
+    ////////// 13 //////////
     // Usando a idade da questão anterior;
-    String validacao_idade;
-    validacao_idade = idade < 18 ? "Menor de idade" : "Maior de idade";
+    String validacao_idade = idade < 18 ? "Menor de idade" : "Maior de idade";
     print(validacao_idade);
 
+    ////////// 14 /////////
     String? nome;
     String nomePadrao = "Não informado";
     String nomeCompleto = nome ?? nomePadrao;
     print(nomeCompleto);
 
-  
+    ////////// 15 ////////////
     final pi = 3.14;
     print(pi);
 /*
--- subtopico_2_3.dart
+++ subtopico_2_3.dart
@@ -2,6 +2,7 @@
 import 'dart:io';
 
 void main(){
+  //////////// 16 /////////////
   print("Qual a sua idade?");
    // O método readLineSync() é um dos métodos usados ​​para receber a entrada do usuário.
   int? idade = int.parse(stdin.readLineSync()!); 
@@ -11,4 +12,61 @@ void main(){
   else{
     print("Menor de idade.");
   }
+
+  /////////// 17 /////////////
+  double nota = 6;
+  
+  if(nota >= 7){
+    print("Aprovado");
+  } else if(nota >= 5 && nota <= 6.9){
+    print("Recuperação");
+  } else{
+    print("Reprovado");
+  }
+
+  /////////// 18 ///////////
+  print("Digite um número inteiro:");
+  int? numero = int.parse(stdin.readLineSync()!); 
+  String exibir = numero % 2 == 0  ? "Par" : "Ímpar";
+  print(exibir);
+
+  /////////// 19 //////////////
+  int dias_da_semana = 1;
+  switch (dias_da_semana) {
+    case 1: {
+      print("Domingo");
+      break;
+    }
+    case 2: {
+      print("Segunda");
+      break;
+    }
+    case 3: {
+      print("Terça");
+      break;
+    }
+    case 4: {
+      print("Quarta");
+      break;
+    }
+    case 5: {
+      print("Quinta");
+      break;
+    }
+    case 6: {
+      print("Sexta");
+      break;
+    }
+    case 7: {
+      print("Sábado");
+      break;
+    }
+   
+    default: {
+      print("Esses são os dias da semana!");
+    }
+    break;
+      
+  }
+  
 }
